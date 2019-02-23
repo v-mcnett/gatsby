032c208a3c1eb92fd8a7eba41cefea3919d75118
Valerie McNett <vmcnett@gmail.com>
2019-02-23 12:29:01 -0800

    testing as stateless component also changing how honeypot is rendered

--- src/pages/BookingForm.js
+++ src/pages/BookingForm.js
@@ -1,50 +1,44 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import SEO from '../components/SEO'
-import Page from '../components/Page'
-import Typography from '@material-ui/core/Typography'
-import withRoot from '../utils/withRoot'
 
-class BookingForm extends React.Component {
-  render() {
-    return (
-      <form
-        name="contact_interop19"
-        method="post"
-        action="/BookingFormSuccess/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="bot-field" />
-        <p>
-          <label>
-            Your Name: <input type="text" name="name" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email: <input type="email" name="email" />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Role:{' '}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message" />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    )
-  }
-}
-export default BookingForm
+export default () => (
+  <form
+    name="contact_interop20"
+    method="post"
+    action="/BookingFormSuccess/"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+  >
+    <p class="hidden">
+      <label>
+        Don’t fill this out if you're human: <input name="bot-field" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Name: <input type="text" name="name" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Email: <input type="email" name="email" />
+      </label>
+    </p>
+    <p>
+      <label>
+        Your Role:{' '}
+        <select name="role[]" multiple>
+          <option value="leader">Leader</option>
+          <option value="follower">Follower</option>
+        </select>
+      </label>
+    </p>
+    <p>
+      <label>
+        Message: <textarea name="message" />
+      </label>
+    </p>
+    <p>
+      <button type="submit">Send</button>
+    </p>
+  </form>
+)
