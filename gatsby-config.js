a3e32a6c1d82c5bf626f49ba258695d2183ec160
Valerie McNett <vmcnett@gmail.com>
2019-03-21 15:26:25 -0700

    changes for manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,21 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-manifest',
-      options: {},
+      options: {
+        name: "Emerald City Photo Booth",
+        short_name: "Emerald City Photo Booth",
+        start_url: "/",
+        background_color: "#5cbc5e",
+        theme_color: "#5cbc5e",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/img/header.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
     },
     'gatsby-plugin-offline',
     {
