39eae9807db6eb0525fd8cdece33b418c2f3c832
Valerie McNett <vmcnett@gmail.com>
2019-10-26 21:56:30 -0700

    Pricing page v2

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,17 +6,25 @@ import Grid from '@material-ui/core/Grid'
 import { withStyles } from '@material-ui/core/styles'
 import './dist/main.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faHeart, faCamera, faHatWizard, faStar, faEnvelope, faPhone, faCalendar, faComment } from '@fortawesome/free-solid-svg-icons'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook, faPinterest, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
 library.add(faHeart)
 library.add(faBars)
+library.add(faCheck)
 library.add(faFacebook)
 library.add(faPinterest)
 library.add(faTwitter)
+library.add(faStar)
+library.add(faCamera)
+library.add(faHatWizard)
+library.add(faEnvelope)
+library.add(faPhone)
+library.add(faCalendar)
+library.add(faComment)
 
 
 const styles = theme => ({
