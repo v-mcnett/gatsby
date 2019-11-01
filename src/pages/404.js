cec0995d89ada6a26f9f7ab784165199ba711603
Valerie McNett <vmcnett@gmail.com>
2019-11-01 12:39:05 -0700

    FIxes to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Page from "../components/Page";
+import { Link } from 'gatsby'
 import Typography from "@material-ui/core/Typography";
 import withRoot from "../utils/withRoot";
 
@@ -7,15 +8,18 @@ class NotFoundPage extends React.Component {
   render() {
     return (
       <Page title="Not Found">
-        <Typography
-          paragraph
-          color="primary"
-          gutterBottom
-          variant="h3"
-          component="div"
-        >
-          Hey! You just hit a page that doesn't exist.
-        </Typography>
+        <div id="content" className="site-content ecpb-site-boundary">
+            <div id="primary" className="content-area">
+              <main id="main" className="site-main">
+        
+              <h4>
+                Sorry, we can't find the page you were looking for.
+              </h4>
+              <p>Return to the <Link to="/">homepage</Link>.</p>
+              </main>
+          </div>
+        </div>
+
       </Page>
     );
   }
